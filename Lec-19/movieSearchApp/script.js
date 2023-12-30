9c5a37ae9873fd587ddbe1f163909c53c7acab69
Shreyance Bodh <shreyancebodh2013@gmail.com>
2023-12-30 14:46:06 +0530

    Lec-19 (movieSearchApp)

---
+++ Lec-19/movieSearchApp/script.js
@@ -0,0 +1,45 @@
+const inp = document.querySelector('input');
+const form = document.querySelector('form');
+const list = document.querySelector('.list');
+
+form.addEventListener('submit', async (e)=>{
+    e.preventDefault();
+    const query = inp.value;
+    inp.value = "";
+    const data = await getData(query);
+    list.innerHTML = '';
+    addToList(data);
+})
+
+// 
+async function getData(query){
+    const res = await fetch(`https://api.tvmaze.com/search/shows?q=${query}`)
+    const data = await res.json();
+    return data;
+}
+
+function addToList(data){
+    console.log(data);
+    data.forEach(item => {
+        const imageURL = item.show.image.medium;
+        const img = document.createElement('img');
+        img.setAttribute('src', imageURL);
+        img.setAttribute('width', '200px')
+        list.append(img);
+    });
+}
+
+
+const leftArrow = document.querySelector('#left-arrow');
+const rightArrow = document.querySelector('#right-arrow');
+
+leftArrow.addEventListener('click', (e)=>{
+    // console.log(window.scrollX)
+    // list.scrollIntoView({})
+    
+})
+
+rightArrow.addEventListener('click', (e)=>{
+    list.scrollIntoView({left:'220px'})
+})
+
